182b4538a132dae374097cda84002479be32349e
Marko <crashxx@gmail.com>
2019-09-07 07:38:35 +0200

    shopify: move to archive add to index

--- archive/shopify/tailwind.config.js
+++ archive/shopify/tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   theme: {
     extend: {
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+        '-72': '-18rem',
+        '-84': '-21rem',
+        '-96': '-24rem'
+      },
       colors: {
         netly: '#00ad9e',
       },
